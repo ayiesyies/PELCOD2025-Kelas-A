e62120acbaae0e6a80b7008fba4c24e2c0e78090
ayiesyies <nuraisyahriskijunita@gmail.com>
2025-09-13 11:34:03 +0700

    PELCOD2025-Kelas-A

---
+++ 250441100041- nur aisyah risky junita/pelcod.py
@@ -0,0 +1,39 @@
+# print("="*50)
+# print("Hello Word")
+# nama = "ayies"
+# print(nama)
+# print(type(nama))
+# boolean = True
+# print(type(boolean))
+
+# penjumblahan = 5+7
+# print("Hasilnya Adalah",penjumblahan)
+
+# nama_asprak = "andika kamal"
+# nama_calon_dosen = nama_asprak
+# print(nama_calon_dosen)
+
+# #tipe data number
+# tahun_lahir = int(input("masukan tahun lahir :"))
+# tahun_sekarang = int(input("masukan tahun sekarang :"))
+
+# perhitungan_usia = tahun_sekarang - tahun_lahir
+# print(perhitungan_usia)
+
+bilangan_1 = 51
+bilangan_2 = 7
+
+modulus = bilangan_1 % bilangan_2
+print(modulus)
+
+# jumlah = int(bilangan)+int(bilangan_2)
+# print(jumlah)
+
+# cek = bilangan > bilangan_2
+# print(cek)
+
+saldo = float(input("masukan saldo awal :")) 
+deposit = float(input("masukan saldo deposit :")) 
+
+saldo += deposit
+print(saldo)
